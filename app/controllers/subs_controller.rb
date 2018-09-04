34f49d86ff914ccd5c17a0a2ee743d94eeafd35d
tprax <tprax01@gmail.com>
2018-09-04 10:27:52 -0600

    add routes

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -8,6 +8,15 @@ class SubsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root "subs#index"
+  resources :subs
+  # resources :subs, only: [:new, :create, :edit, :update]
+  # resources :subs, except: [:index, :show, :delete]
+
+  # get "/subs", to: "subs#index"
+  # get "/subs/:id", to: "subs#show"
+  # delete "/subs/:id", to: "subs#destroy" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
